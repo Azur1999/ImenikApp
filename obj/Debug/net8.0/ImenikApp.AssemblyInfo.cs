2ee8518afb790133d1b09707ee216dda24a5f7cc
Azur Jusic <azurjusic99@gmail.com>
2025-03-18 16:46:05 +0100

    buildFix

--- obj/Debug/net8.0/ImenikApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ImenikApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImenikApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9865561b18dabc6fd96d3e6dc013732791acd006")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c45200c1ef25ea14c785c3ef23377e13d3ff90e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImenikApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImenikApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
