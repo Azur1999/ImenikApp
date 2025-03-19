5ed221fa19a43a12980f46bb8fa482127c4da41d
Azur Jusic <azurjusic99@gmail.com>
2025-03-19 08:52:17 +0100

    nove promjene

---
+++ DTO/OsobaPutRequestDTO.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using ImenikApp.Models;
+
+namespace ImenikApp.DTO {
+    public class OsobaPutRequestDTO {
+        public int OsobaId { get; set; }
+        [StringLength(50)]
+        public required string Ime { get; set; }
+        [StringLength(50)]
+        public required string Prezime { get; set; }
+        [RegularExpression(@"^\d{3}/\d{3}-\d{3}$")]
+        public required string BrojTelefona { get; set; }
+        public required  PolEnum Pol { get; set; }
+        [EmailAddress]
+        public required string Email { get; set; }
+        public string? NazivGrad { get; set; } 
+        public string? NazivDrzava { get; set; } 
+        public required DateOnly DatumRodjenja { get; set; }
+        public int? Starost {get; set;} 
+    }
+}
